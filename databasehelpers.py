5d1dd2a3e5f74c66b034a5f89499047d6c36ebb5
HecticBanzai <larryling.main@gmail.com>
2022-09-03 12:19:31 -0500

    Update databasehelpers.py

--- databasehelpers.py
+++ databasehelpers.py
@@ -7,7 +7,6 @@ from event import *
 
 async def add_event_object(guild_id: int, event_object: event):
   con = await asyncpg.connect(os.getenv("DATABASE_URL"))
-  # con = await asyncpg.connect(database="deadline", user="postgres", password="larry1014")
 
   await con.execute(f"""INSERT INTO events_{guild_id} (event_name, event_deadline_str, send_to_channel_id, description, job_id, users_opted_in)
                         VALUES ($1, $2, $3, $4, $5, $6)""",
@@ -21,14 +20,12 @@ async def add_event_object(guild_id: int, event_object: event):
 
 async def remove_event_object(guild_id: int, event_name: str):
   con = await asyncpg.connect(os.getenv("DATABASE_URL"))
-  # con = await asyncpg.connect(database="deadline", user="postgres", password="larry1014")
 
   await con.execute(f"""DELETE FROM events_{guild_id} WHERE event_name='{event_name}';""")
   await con.close()
 
 async def update_attendance_list(guild_id: int, user_id_list: list, event_name: str):
   con = await asyncpg.connect(os.getenv("DATABASE_URL"))
-  # con = await asyncpg.connect(database="deadline", user="postgres", password="larry1014")
 
   await con.execute(f"""UPDATE events_{guild_id}
                         SET users_opted_in = $1
